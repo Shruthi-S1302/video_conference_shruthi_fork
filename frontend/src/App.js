4becc107548853808f9f38fe284208966885cb8f
Biswadeep Ray <biswa7102raydeep@gmail.com>
2022-06-03 20:34:43 +0530

    created SocketContext

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,50 @@
 
 import {AppBar, Typography} from '@material-ui/core';
+import VideoPlayer from './Components/VideoPlayer';
+import Options from './Components/Options';
+import Notifications from './Components/Notifications';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme)=>({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+    },
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <>
-  <AppBar position="static" color="inherit">
+    <div className={classes.wrapper}>
+  <AppBar className={classes.appBar} position="static" color="inherit">
     <Typography variant="h4" align="center">
       Video Conference
     </Typography>
   </AppBar>
-</>
+  <VideoPlayer/>
+  <Options>
+    <Notifications/>
+  </Options>
+</div>
   );
 }
 
