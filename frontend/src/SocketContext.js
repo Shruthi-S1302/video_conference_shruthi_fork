0b4cba2d5abae3057bb153778456dc972f15d84b
Biswadeep Ray <biswa7102raydeep@gmail.com>
2022-06-05 15:50:09 +0530

    deploy process update

--- frontend/src/SocketContext.js
+++ frontend/src/SocketContext.js
@@ -5,7 +5,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000');
+const socket = io('https://video-conf002.herokuapp.com/');
 
 const ContextProvider = ({children}) => {
 
